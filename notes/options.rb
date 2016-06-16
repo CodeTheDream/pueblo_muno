49cfaa7f24ab24c084be399d7695e249a235bd55
Cruz Nunez <nunez.a.cruz@gmail.com>
2016-06-16 16:32:48 -0400

    First commit

---
+++ notes/options.rb
@@ -0,0 +1,95 @@
+options = {
+  'Policy Change' => {
+    'entree' => {
+      'Raleigh' => 35,
+      'Wake County' => '+20',
+      'Wake + surrounding counties' => '+25',
+      'Statewide' => '+30',
+      'Federal' => '+35'
+    },
+    'dessert' => {
+      'Raleigh' => 25,
+      'Wake County' => '+15',
+      'Wake + surrounding counties' => '+20',
+      'Statewide' => '+25',
+      'Federal' => '+30'
+    },
+    'drink' => {
+      'Raleigh' => 15,
+      'Wake County' => '+10',
+      'Wake + surrounding counties' => '+15',
+      'Statewide' => '+20',
+      'Federal' => '+25'
+    }
+  },
+  'Individual Growth' => {
+    'entree' => {
+      'Raleigh' => 25,
+      'Wake County' => '+20',
+      'Wake + surrounding counties' => '+25',
+      'Statewide' => '+30'
+    },
+    'dessert' => {
+      'Raleigh' => 15,
+      'Wake County' => '+15',
+      'Wake + surrounding counties' => '+20',
+      'Statewide' => '+25'
+    },
+    'drink' => {
+      'Raleigh' => 10,
+      'Wake County' => '+10',
+      'Wake + surrounding counties' => '+15',
+      'Statewide' => '+20'
+    }
+  },
+  'Individual Growth and Take Action' => {
+    'entree' => {
+      'Raleigh' => 25,
+      'Wake County' => '+20',
+      'Wake + surrounding counties' => '+25',
+      'Statewide' => '+30'
+    },
+    'dessert' => {
+      'Raleigh' => 15,
+      'Wake County' => '+15',
+      'Wake + surrounding counties' => '+20',
+      'Statewide' => '+25'
+    },
+    'drink' => {
+      'Raleigh' => 10,
+      'Wake County' => '+10',
+      'Wake + surrounding counties' => '+15',
+      'Statewide' => '+20'
+    }
+  },
+  'Referrals' => {
+    'entree' => {},
+    'dessert' => {
+      'Raleigh' => 10,
+      'Wake County' => '+10',
+      'Wake + surrounding counties' => '+15',
+      'Statewide' => '+20'
+    },
+    'drink' => {
+      'Raleigh' => 5,
+      'Wake County' => '+5',
+      'Wake + surrounding counties' => '+10',
+      'Statewide' => '+15'
+    }
+  },
+  'Cultural Diversity' => {
+    'entree' => {},
+    'dessert' => {
+      'Raleigh' => 10,
+      'Wake County' => '+10',
+      'Wake + surrounding counties' => '+15',
+      'Statewide' => '+20'
+    },
+    'drink' => {
+      'Raleigh' => 5,
+      'Wake County' => '+5',
+      'Wake + surrounding counties' => '+10',
+      'Statewide' => '+15'
+    }
+  }
+}
